469a0275e609a8039e6dc75ce4ceba1e0c1312aa
Erik Roberts <erik@bugg.co>
2022-03-25 16:44:15 -0400

    Fixed issue with wrong postgres database being used

--- backend/api.js
+++ backend/api.js
@@ -5,13 +5,22 @@ const pool = require('./db');
 router = express.Router();
 
 router.get("/score/*", async (req, res) => {
+	// TODO: This URL does not include query parameters, and perhaps URL anchors
+	const passedUrl = req.params.url;
 	// URLs are hashed for privacy -- URLs can contain private info, particularly in the query string.
-	const urlHash = crypto.createHash('sha256', req.params.url).digest().toString('hex');
-	const apiUrl = `https://ipqualityscore.com/api/json/url/${process.env.API_KEY}/${req.params.url}`;
+	const urlHash = crypto.createHash('sha256', passedUrl).digest().toString('hex');
+	const apiUrl = `https://ipqualityscore.com/api/json/url/${process.env.API_KEY}/${passedUrl}`;
 
-	console.log(req.params[0]);
-	//const result = await fetch(apiUrl);
+	const connection = await pool.connect();
+	const cachedResult = await connection.query('SELECT score FROM scan_cache WHERE urlHash=$1', [urlHash]);
+	if(cachedResult.rowCount > 0) {
+		// TODO return cached result
+		await connection.release();
+		return;
+	}
 
+	const apiResult = await fetch(apiUrl);
+	await connection.release();
 });
 
 module.exports = router;
