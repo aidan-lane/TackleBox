ba1b57deed4841d116d5aeff490b2ca30300a289
Erik Roberts <erik@bugg.co>
2022-03-28 22:16:43 -0400

    Add a few more comments

--- backend/api.js
+++ backend/api.js
@@ -10,8 +10,11 @@ router.get("/score/*", async (req, res) => {
 	const urlHash = crypto.createHash('sha256').update(passedUrl).digest('hex');
 	const apiUrl = `https://ipqualityscore.com/api/json/url/${process.env.API_KEY}/${passedUrl}`;
 
+	// Connection pulled out of pool instead of using pool.query() since we might make multiple queries.
 	const connection = await pool.connect();
 	const cachedResult = await connection.query('SELECT * FROM scan_cache WHERE hash=$1', [urlHash]);
+	// If we have already scanned this URL then we just can return the cached response from before.
+	//  TODO: Potentially make this expire in the future?
 	if(cachedResult.rowCount > 0) {
 		await connection.release();
 		res.json(cachedResult.rows[0]);
@@ -20,6 +23,7 @@ router.get("/score/*", async (req, res) => {
 
 	const apiResult = await (await fetch(apiUrl)).json();
 	if(apiResult.success) {
+		// Reformatted data to include only the relevant information for our database/end user
 		const ourData = {
 			hash: urlHash,
 			score: apiResult.risk_score,
@@ -32,19 +36,19 @@ router.get("/score/*", async (req, res) => {
 			category: apiResult.category
 		};
 
+		// Insert data to DB and send same data to user
 		await connection.query('INSERT INTO scan_cache (hash, score, parking, spam, malware, phishing, suspicious, adult, category) VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9)',
 			[ourData.hash, ourData.score, ourData.parking, ourData.spam, ourData.malware, ourData.phishing,
 				ourData.suspicious, ourData.adult, ourData.category]);
-
 		await connection.release();
+
 		res.json(ourData);
 	} else {
+		// Some sort of error from the API. We don't tell the user what the error was.
 		await connection.release();
 		res.status(502);
 		console.error('IPQualityScore error:', apiResult.message, 'Request ID:', apiResult.request_id);
-		res.json({
-			error: 'Gateway error. Contact a developer of TackleBox for more info.'
-		});
+		res.json({ error: 'Gateway error. Contact a developer of TackleBox for more info.' });
 	}
 });
 
