a81d9b359b505987465ad3fd61ad99cf9e57080f
crowlc3 <45721609+crowlc3@users.noreply.github.com>
2022-03-28 23:39:25 -0400

    color selector

--- frontend/slider.js
+++ frontend/slider.js
@@ -5,12 +5,15 @@ var rangeValue = function(){
   var target = document.querySelector('.value');
   if (newValue == 0){
     target.innerHTML = "low";
+    elem.style.setProperty('--sliderColor', "#3AD06C");
   }
   if (newValue == 1){
     target.innerHTML = "med";
+    elem.style.setProperty('--sliderColor', "#E4B635");
   }
   if (newValue == 2){
     target.innerHTML = "high";
+    elem.style.setProperty('--sliderColor', "#BD1515");
   }
 }
 
